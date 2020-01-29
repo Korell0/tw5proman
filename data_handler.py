905351221492b4afacdf5a2ec9863d2137580a26
Korell0 <korsosadam97@gmail.com>
2020-01-29 11:45:05 +0100

    remelem_jo

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,7 @@
 import persistence
 import database_common
+import bcrypt
+
 
 @database_common.connection_handler
 def insert_board_data(cursor,id,title):
@@ -92,3 +94,64 @@ def get_cards_for_board(board_id):
             card['status_id'] = get_card_status(card['status_id'])  # Set textual status for the card
             matching_cards.append(card)
     return matching_cards
+
+
+@database_common.connection_handler
+def get_usernames_from_database(cursor):
+    cursor.execute("""
+                    SELECT user_name FROM users
+                    """,)
+    names = cursor.fetchall()
+    return [item["user_name"] for item in names]
+
+
+@database_common.connection_handler
+def get_username_by_user_id(cursor,user_id):
+    cursor.execute("""
+                SELECT user_name FROM users
+                WHERE id = %(user_id)s
+    """,
+                   {"user_id":user_id})
+    username = cursor.fetchone()
+    return username
+
+@database_common.connection_handler
+def get_hash_from_database(cursor,username):
+    cursor.execute("""
+                SELECT hashed_password FROM users
+                WHERE user_name = %(username)s
+    """,
+                   {"username": username})
+    hash = cursor.fetchone()
+    return hash
+
+
+def get_hash_from_password(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    decoded_hash = hashed_password.decode('utf-8')
+    return decoded_hash
+
+
+def verify_password(password, hash):
+    hashed_bytes_password = hash.encode('utf-8')
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_bytes_password)
+
+
+@database_common.connection_handler
+def registration(cursor,username,password):
+    hashed_bytes = get_hash_from_password(password)
+    cursor.execute("""
+                    INSERT INTO users (user_name,hashed_password)
+                    VALUES (%(username)s,%(hashed_bytes)s);
+                   """,
+                   {"username": username,
+                    "hashed_bytes": hashed_bytes})
+
+
+@database_common.connection_handler
+def get_username_by_user_id(cursor,userid):
+    cursor.execute("""
+                SELECT user_name FROM users
+                WHERE id = %(userid)s
+    """,
+                   {"userid": userid})
--- main.py
+++ main.py
@@ -1,35 +1,56 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for,session,request,make_response,redirect
 from util import json_response
 
 import data_handler
 
 app = Flask(__name__)
+app.secret_key = '12345'
+
+
+@app.route('/set-cookie')
+def cookie_insertion():
+    redirect_to_index = redirect('/')
+    response = make_response(redirect_to_index)
+    response.set_cookie('cookie-name', value='values')
+    return response
+
+
+@app.route('/registration',methods=["GET","POST"])
+def registration():
+    if request.method == "POST":
+        if " " not in request.form["username"] and " " not in request.form["password"] \
+                and request.form["username"] is not "" and request.form["password"] is not "":
+            if request.form["username"] in data_handler.get_usernames_from_database():
+                error = "This username is already in use"
+                return render_template("index.html", error=error)
+            data_handler.registration(request.form["username"], request.form["password"])
+        error = "Wrong characters..."
+        return render_template("index.html", error=error)
+
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method == "POST":
+        if data_handler.get_hash_from_database(request.form["username"]) is not None:
+            database_password = data_handler.get_hash_from_database(request.form["username"])
+            verify_password = data_handler.verify_password(request.form["password"], database_password["hashed_password"])
+            if verify_password:
+                session["username"] = request.form["username"]
+                print(session["username"])
+                return render_template("index.html", username=session["username"])
+        error = "Invalid username or password"
+        return render_template("index.html", error=error)
+    return render_template("index.html")
 
 
 @app.route("/")
 def index():
-    # data_handler.insert_board_data(1,"Board 1")
-    # data_handler.insert_board_data(2, "Board 2")
-    # data_handler.insert_card_data(1,1,"new card 1",0,0)
-    # data_handler.insert_card_data(2,1,"new card 2",0,1)
-    # data_handler.insert_card_data(3,1,"in progress card",1,0)
-    # data_handler.insert_card_data(4,1,"planning",2,0)
-    # data_handler.insert_card_data(5,1,"done card 1",3,0)
-    # data_handler.insert_card_data(6,1,"done card 1",3,1)
-    # data_handler.insert_card_data(7,2,"new card 1",0,0)
-    # data_handler.insert_card_data(8,2,"new card 2",0,1)
-    # data_handler.insert_card_data(9,2,"in progress card",1,0)
-    # data_handler.insert_card_data(10,2,"planning",2,0)
-    # data_handler.insert_card_data(11,2,"done card 1",3,0)
-    # data_handler.insert_card_data(12,2,"done card 1",3,1)
-    # data_handler.insert_status_data(0, "new")
-    # data_handler.insert_status_data(1,"in progress")
-    # data_handler.insert_status_data(2, "testing")
-    # data_handler.insert_status_data(3,"done")
     """
     This is a one-pager which shows all the boards and cards
     """
-    return render_template('index.html')
+    session["username"] = None
+    username = session["username"]
+    return render_template('index.html', username=username)
 
 
 @app.route("/get-boards")
