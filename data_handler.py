f5d724b54af64058129b4ab7ecdf5bb2adacc657
Noemi <noemiboroka+gh@gmail.com>
2020-02-11 13:49:30 +0100

    Add new board button implemented

--- data_handler.py
+++ data_handler.py
@@ -29,6 +29,8 @@ def get_usernames_from_database():
 def registration(username,password):
     return persistance.registration(username,password)
 
+def create_new_board():
+    return persistance.create_new_board()
 
 def get_hash_from_database(username):
     return persistance.get_hash_from_database(username)
